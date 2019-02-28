d885297542f0b3d9855d5799d664feffa0c11d32
Larry Simiyu <larrysimiyu@Lambda-School-Loaner-77s-MacBook-Pro.local>
2019-02-27 19:07:55 -0800

    day mvp done

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,24 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+import Friend from './Friend';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      friends: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/friends')
+    .then(res => this.setState({friends: res.data}))
+    .catch(err => console.long(err + 'something went wrong'));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.friends.map((friend)=>{
+        return(
+          <Friend friend={friend} key={friend.id}/>
+        );
+      })}
       </div>
     );
   }
